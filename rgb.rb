845aee41ad23ad62ecc67cd99c35791f6b0b036b
EC2 Default User <ec2-user@ip-172-31-35-179.ec2.internal>
2022-03-07 08:56:57 +0000

    4.6読了

--- rgb.rb
+++ rgb.rb
@@ -1,10 +1,52 @@
-def to_hex(r, g, b)
+def to_hex_old1(r, g, b)
   '#' +
     r.to_s(16).rjust(2, '0') +
     g.to_s(16).rjust(2, '0') +
     b.to_s(16).rjust(2, '0')
 end
 
+
+def to_hex_old2(r, g, b)
+  hex = '#'
+  [r, g, b].each do |n|
+    hex += n.to_s(16).rjust(2, '0')
+  end
+  hex
+end
+
+def to_hex(r, g, b)
+  [r, g, b].sum('#') do |n|
+    n.to_s(16).rjust(2, '0')
+  end
+end
+
+def to_ints_old(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  ints = []
+  [r, g, b].each do |s|
+    ints << s.hex
+  end
+  ints
+end
+
+def to_ints(hex)
+  r, g, b = hex[1..2], hex[3..4], hex[5..6]
+  [r, g, b].map do |s|
+    s.hex
+  end
+end
+
+
+
+
 p to_hex(0, 0, 0)
 p to_hex(255, 255, 255)
 p to_hex(4, 60, 120)
+
+
+p to_ints(to_hex(0, 0, 0))
+p to_ints(to_hex(255, 255, 255))
+p to_ints(to_hex(4, 60, 120))
+
