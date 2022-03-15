ba73ed4a64c2682cb3a27f46ff00d6b9102a7cfb
EC2 Default User <duc.de.alphaasile@gmail.com>
2022-03-15 00:10:21 +0000

    3章読了

---
+++ test/fizz_buzz_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require_relative '../lib/fizz_buzz'
+
+class FizzBuzzTest < Minitest::Test
+  def test_fizz_buzz
+    assert_equal '1', fizz_buzz(1)
+    assert_equal '2', fizz_buzz(2)
+    assert_equal 'Fizz', fizz_buzz(3)
+    assert_equal 'FizzBuzz', fizz_buzz(15)
+  end
+end
